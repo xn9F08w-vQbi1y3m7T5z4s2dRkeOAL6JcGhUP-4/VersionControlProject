8bfefd03cb2bf5269adb80cfbf9224373a4478bd
xn9F08w-vQbi1y3m7T5z4s2dRkeOAL6JcGhUP-4 <148507528+xn9F08w-vQbi1y3m7T5z4s2dRkeOAL6JcGhUP-4@users.noreply.github.com>
2023-10-25 15:55:58 -0700

    first

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+# tyler squires
+
+def encode(code):
+    encoded = ""
+    for char in code:
+        new = int(char) + 3
+        if new > 9:
+            encoded += str(new)[1]
+        else:
+            encoded += str(new)
+    return encoded
+
 if __name__ == '__main__':
-    pass
+    encoded = ""
+    decoded = ""
+    while True:
+        option = input("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n\nPlease enter an option: ")
+        if int(option) == 3:
+            break
+        if int(option) == 2:
+            print(f"The encoded password is {encoded}, and the original password is {decoded}\n")
+        if int(option) == 1:
+            code = input("Please enter your password to encode: ")
+            decoded = code
+            encoded = encode(code)
+            print("Your password has been encoded and stored!\n")
